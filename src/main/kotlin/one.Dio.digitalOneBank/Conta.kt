b2cf7e413181201aac402c390dc284d778c7a97c
Milton-Luis <miltonluis138@gmail.com>
2021-07-12 16:47:09 -0300

    Nova classe: Conta

---
+++ src/main/kotlin/one.Dio.digitalOneBank/Conta.kt
@@ -0,0 +1,18 @@
+package one.Dio.digitalOneBank
+
+import java.math.BigDecimal
+
+
+class Conta(
+    val agencia:String, 
+    val numConta:String, 
+    val saldo:BigDecimal){
+    
+    fun deposito(valor:BigDecimal){
+
+    }
+
+    fun saque(valor:BigDecimal){
+        
+    }
+}
