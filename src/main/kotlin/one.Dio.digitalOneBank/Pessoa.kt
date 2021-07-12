8757e3dea36f1fbddee5109eb6badd937453acad
Milton-Luis <miltonluis138@gmail.com>
2021-07-12 16:19:42 -0300

    Aplicando o private set

--- src/main/kotlin/one.Dio.digitalOneBank/Pessoa.kt
+++ src/main/kotlin/one.Dio.digitalOneBank/Pessoa.kt
@@ -3,15 +3,11 @@ package one.Dio.digitalOneBank
 class Pessoa{
     var nome: String = "Milton"
     var cpf:String = "187.452.098-02"
-
-    inner class Endereco(){
-        var rua:String = "Rua Alameda dos Anjos - 762"
-    }
+    private set //permite a visibilidade porém em nenhum outra instancia ele poderá ser alterado
 }
 
 fun main(){
     val pessoa = Pessoa()
     println(pessoa.nome)
     println(pessoa.cpf)
-    println(pessoa.Endereco().rua)
 }
