b30a46ae79c2da41637d7a6199c576143dbe3fdd
Milton-Luis <miltonluis138@gmail.com>
2021-07-12 16:31:27 -0300

    Usando o construtor e interpolação

--- src/main/kotlin/one.Dio.digitalOneBank/Pessoa.kt
+++ src/main/kotlin/one.Dio.digitalOneBank/Pessoa.kt
@@ -4,10 +4,16 @@ class Pessoa{
     var nome: String = "Milton"
     var cpf:String = "187.452.098-02"
     private set //permite a visibilidade porém em nenhum outra instancia ele poderá ser alterado
+
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome" + ", CPF: $cpf"  //interpolação
 }
 
 fun main(){
     val pessoa = Pessoa()
-    println(pessoa.nome)
-    println(pessoa.cpf)
+    // println(pessoa.nome)
+    // println(pessoa.cpf)
+
+    println(pessoa.pessoaInfo())
 }
